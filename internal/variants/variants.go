bf5548b24c4857a20062ecfc1b4917dbfbc1640b
Ana Manolache <anamano@google.com>
2018-09-21 14:53:44 -0400

    Revert "Refactor non-coordinate parameters"
    This reverts commit bba32e686f2551229b19af1db195b81dadfa6b47.

--- internal/variants/variants.go
+++ internal/variants/variants.go
@@ -19,34 +19,19 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"regexp"
 	"strings"
 
 	"cloud.google.com/go/bigquery"
 )
 
-var basesRegex = regexp.MustCompile(`^([ACGT]+|N)$`)
-
 // Query holds information about a single query against a Beacon.
 type Query struct {
-	// ReferenceName is the chromosome reference name.
-	ReferenceName string
-	// ReferenceBases is the allele reference base.
-	ReferenceBases string
-	// AlternateBases is the allele alternate bases.
-	AlternateBases string
-	// Start matches the alleles that start at this position.
-	Start *int64
-	// End matches the alleles that end at this position.
-	End *int64
-	// StartMin matches the alleles that start at this position or higher.
-	StartMin *int64
-	// StartMax matches the alleles that start at this position or lower.
-	StartMax *int64
-	// EndMin matches the alleles that end at this position or higher.
-	EndMin *int64
-	// EndMax matches the alleles that end at this position or lower.
-	EndMax *int64
+	// RefName is the chromosome reference name.
+	RefName string
+	// Allele is the allele reference base.
+	Allele string
+	// Coord is the coordinate that intersects the retrieved alleles.
+	Coord *int64
 }
 
 // Execute queries the allele database with the Query parameters.
@@ -80,41 +65,14 @@ func (q *Query) Execute(ctx context.Context, projectID, tableID string) (bool, e
 
 // ValidateInput validates the Query parameters meet the ga4gh beacon api requirements.
 func (q *Query) ValidateInput() error {
-	if q.ReferenceName == "" {
-		return errors.New("missing reference name")
+	if q.RefName == "" {
+		return errors.New("missing chromosome name")
 	}
-	if q.ReferenceBases == "" {
-		return errors.New("missing reference bases")
+	if q.Allele == "" {
+		return errors.New("missing allele")
 	}
-	if !basesRegex.MatchString(q.ReferenceBases) {
-		return errors.New("invalid value for reference bases")
-	}
-	if q.AlternateBases != "" && !basesRegex.MatchString(q.AlternateBases) {
-		return errors.New("invalid value for alternate bases")
-	}
-	if err := q.validateCoordinates(); err != nil {
-		return fmt.Errorf("validating coordinates: %v", err)
-	}
-	return nil
-}
-
-func (q *Query) validateCoordinates() error {
-	var precisePosition, imprecisePosition bool
-	if q.Start != nil && (q.End != nil || q.ReferenceBases != "") {
-		precisePosition = true
-	}
-	if q.StartMin != nil && q.StartMax != nil && q.EndMin != nil && q.EndMax != nil {
-		imprecisePosition = true
-	}
-
-	if precisePosition && imprecisePosition {
-		return errors.New("please query either precise or imprecise position")
-	}
-	if precisePosition || imprecisePosition {
-		return nil
-	}
-	if q.Start != nil && q.End != nil || q.StartMin != nil || q.StartMax != nil || q.EndMin != nil || q.EndMax != nil {
-		return errors.New("restrictions not met for provided coordinates")
+	if q.Coord == nil {
+		return errors.New("missing coordinate")
 	}
 	return nil
 }
@@ -129,25 +87,11 @@ func (q *Query) whereClause() string {
 			add("%s='%s'", dbColumn, value)
 		}
 	}
-
-	simpleClause("reference_name", q.ReferenceName)
-	simpleClause("reference_bases", q.ReferenceBases)
-	if q.AlternateBases != "" {
-		add(fmt.Sprintf("(SELECT count(*) FROM UNNEST(alternate_bases) AS alt WHERE alt IN ('%s') ) > 0", q.AlternateBases))
+	simpleClause("reference_name", q.RefName)
+	simpleClause("reference_bases", q.Allele)
+	// Start is inclusive, End is exclusive.  Search exactly for coordinate.
+	if q.Coord != nil {
+		add("v.start <= %d AND %d < v.end", *q.Coord, *q.Coord+1)
 	}
-	q.bqCoordinatesToWhereClause(add)
 	return strings.Join(clauses, " AND ")
-}
-
-func (q *Query) bqCoordinatesToWhereClause(add func(format string, args ...interface{})) {
-	if q.Start != nil {
-		if q.End != nil {
-			add("v.start = %d AND %d = v.end", *q.Start, *q.End)
-		} else {
-			add("v.start = %d", *q.Start)
-		}
-	}
-	if q.StartMin != nil && q.StartMax != nil && q.EndMin != nil && q.EndMax != nil {
-		add("%d <= v.start AND v.start <= %d AND %d <= v.end AND v.end <= %d", *q.StartMin, *q.StartMax, *q.EndMin, *q.EndMax)
-	}
 }
